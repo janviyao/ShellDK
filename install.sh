540cca27f3e6af7653d50810de7dc2c0b0bacebb
janvizw <9971289@qq.com>
2018-12-23 17:36:42 +0800

    modify install.sh

--- install.sh
+++ install.sh
@@ -5,8 +5,7 @@ HOME_DIR=`pwd`/
 CAMP_DIR=${CUR_DIR#${HOME_DIR}}
 OPTYPE=$1
 
-if [ "${OPTYPE}" = "clean" ]
-then
+if [ "${OPTYPE}" = "clean" ]; then
     # remove current environment
     rm -fr ~/.vim
     rm -fr ~/.vimSession
@@ -20,8 +19,7 @@ then
     exit
 fi
 
-if [ "${OPTYPE}" = "vim" ]
-then
+if [ "${OPTYPE}" = "vim" -o "${OPTYPE}" = "all" ]; then
     # install vim
     cd ${CUR_DIR}/tools
     IS_INSTALL=`rpm -qa | grep readline-devel`
@@ -60,8 +58,7 @@ then
     exit
 fi
 
-if [ "${OPTYPE}" = "tig" ]
-then
+if [ "${OPTYPE}" = "tig" -o "${OPTYPE}" = "all" ]; then
     # install tig
     cd ${CUR_DIR}/tools
     tar -xzf libiconv-1.15.tar.gz
@@ -100,70 +97,76 @@ then
 fi
 
 # prepare environment
-if [ ! -f ~/.vimrc ]; then
-    ln -s ${CAMP_DIR}/vimrc ~/.vimrc
-fi
+if [ "${OPTYPE}" = "env" -o "${OPTYPE}" = "all" ]; then
+    if [ ! -f ~/.vimrc ]; then
+        ln -s ${CAMP_DIR}/vimrc ~/.vimrc
+    fi
 
-if [ ! -f ~/.bashrc ]; then
-    ln -s ${CAMP_DIR}/bashrc ~/.bashrc
-fi
+    if [ ! -f ~/.bashrc ]; then
+        ln -s ${CAMP_DIR}/bashrc ~/.bashrc
+    fi
 
-if [ ! -f ~/.bash_profile ]; then
-    ln -s ${CAMP_DIR}/bash_profile ~/.bash_profile
-fi
+    if [ ! -f ~/.bash_profile ]; then
+        ln -s ${CAMP_DIR}/bash_profile ~/.bash_profile
+    fi
 
-if [ ! -f ~/.minttyrc ]; then
-    ln -s ${CAMP_DIR}/minttyrc ~/.minttyrc
-fi
+    if [ ! -f ~/.minttyrc ]; then
+        ln -s ${CAMP_DIR}/minttyrc ~/.minttyrc
+    fi
 
-if [ ! -f ~/.inputrc ]; then
-    ln -s ${CAMP_DIR}/inputrc ~/.inputrc
-fi
+    if [ ! -f ~/.inputrc ]; then
+        ln -s ${CAMP_DIR}/inputrc ~/.inputrc
+    fi
 
-if [ ! -f ~/.astylerc ]; then
-    ln -s ${CAMP_DIR}/astylerc ~/.astylerc
-fi
+    if [ ! -f ~/.astylerc ]; then
+        ln -s ${CAMP_DIR}/astylerc ~/.astylerc
+    fi
 
-# source environment config
-if [ -f ~/.bashrc ]; then
-    source ~/.bashrc
-fi
+    # source environment config
+    if [ -f ~/.bashrc ]; then
+        source ~/.bashrc
+    fi
 
-if [ -f ~/.bash_profile ]; then
-    source ~/.bash_profile
-fi
+    if [ -f ~/.bash_profile ]; then
+        source ~/.bash_profile
+    fi
 
-if [ -f ~/.minttyrc ]; then
-    source ~/.minttyrc
-fi
+    if [ -f ~/.minttyrc ]; then
+        source ~/.minttyrc
+    fi
 
-# build vim-work environment
-mkdir -p ~/.vim
-cp -fr ${CAMP_DIR}/colors ~/.vim
-cp -fr ${CAMP_DIR}/syntax ~/.vim
+    # build vim-work environment
+    mkdir -p ~/.vim
+    cp -fr ${CAMP_DIR}/colors ~/.vim
+    cp -fr ${CAMP_DIR}/syntax ~/.vim
+
+    # install bundle plugin
+    if [ ! -d ~/.vim/bundle/vundle ]; then
+        git clone https://github.com/gmarik/vundle.git ~/.vim/bundle/vundle
+    fi
+fi
 
 # install astyle
-cd ${CUR_DIR}/tools
-tar -xzf astyle_*.tar.gz
+if [ "${OPTYPE}" = "astyle" -o "${OPTYPE}" = "all" ]; then
+    cd ${CUR_DIR}/tools
+    tar -xzf astyle_*.tar.gz
 
-cd astyle/build/gcc
-make
-if [$? -ne 0 ]; then
-    exit -1
+    cd astyle/build/gcc
+    make
+    if [$? -ne 0 ]; then
+        exit -1
+    fi
+
+    cp -f bin/astyle* /usr/bin/
+    chmod 777 /usr/bin/astyle*
+
+    cd ${CUR_DIR}/tools
+    rm -fr astyle/
 fi
 
-cp -f bin/astyle* /usr/bin/
-chmod 777 /usr/bin/astyle*
-
-cd ${CUR_DIR}/tools
-rm -fr astyle/
-
 # install ack-grep
-cd ${CUR_DIR}/tools
-cp -f ack-* /usr/bin/ack-grep
-chmod 777 /usr/bin/ack-grep
-
-# install bundle plugin
-if [ ! -d ~/.vim/bundle/vundle ]; then
-    git clone https://github.com/gmarik/vundle.git ~/.vim/bundle/vundle
+if [ "${OPTYPE}" = "astyle" -o "${OPTYPE}" = "all" ]; then
+    cd ${CUR_DIR}/tools
+    cp -f ack-* /usr/bin/ack-grep
+    chmod 777 /usr/bin/ack-grep
 fi
