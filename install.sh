4cf0c6e32873eb8ab6c47bb853ed8f53baf321e6
GazeMoon <9971289@qq.com>
2018-12-31 23:26:24 +0800

    modify install.sh

--- install.sh
+++ install.sh
@@ -1,8 +1,5 @@
 #set -x
-CUR_DIR=`pwd`
-cd ~
-HOME_DIR=`pwd`/
-CAMP_DIR=${CUR_DIR#${HOME_DIR}}
+ROOT_DIR=$(cd `dirname $0`;pwd)
 OPTYPE=$1
 
 # remove current environment
@@ -20,102 +17,146 @@ fi
 
 # install vim
 if [ "${OPTYPE}" = "vim" -o "${OPTYPE}" = "all" ]; then
-    cd ${CUR_DIR}/tools
+    cd ${ROOT_DIR}/tools
     IS_INSTALL=`rpm -qa | grep readline-devel`
     if [ -z "${IS_INSTALL}" ]; then
         rpm -ivh readline-devel-6.2-9.el7.x86_64.rpm --nodeps --force
+        if [ $? -ne 0 ]; then
+            exit -1
+        fi
     fi
 
     IS_INSTALL=`rpm -qa | grep ncurses-devel`
     if [ -z "${IS_INSTALL}" ]; then
         rpm -ivh ncurses-devel-5.9-13.20130511.el7.x86_64.rpm --nodeps --force
+        if [ $? -ne 0 ]; then
+            exit -1
+        fi
     fi
 
     IS_INSTALL=`rpm -qa | grep lua-devel`
     if [ -z "${IS_INSTALL}" ]; then
         rpm -ivh lua-devel-5.1.4-14.el7.x86_64.rpm --nodeps --force
+        if [ $? -ne 0 ]; then
+            exit -1
+        fi
     fi
 
     #tar -xzf lua-5.3.3.tar.gz
     #cd lua-5.3.3
     #make linux && make install
 
-    #cd ${CUR_DIR}/tools
+    #cd ${ROOT_DIR}/tools
     #rm -fr lua-5.3.3
 
     tar -xzf vim-8.1.0152.tar.gz
     cd vim-8.1.0152
     ./configure --prefix=/usr --with-features=huge --enable-cscope --enable-multibyte --enable-fontset --enable-largefile --enable-luainterp=yes --enable-pythoninterp=yes --disable-gui --disable-netbeans 
-    make && make install
     if [ $? -ne 0 ]; then
         exit -1
     fi
 
-    cd ${CUR_DIR}/tools
+    make -j 2
+    if [ $? -ne 0 ]; then
+        exit -1
+    fi
+
+    make install
+    if [ $? -ne 0 ]; then
+        exit -1
+    fi
+
+    cd ${ROOT_DIR}/tools
     rm -fr vim-8.1.0152
 fi
 
 # install tig
 if [ "${OPTYPE}" = "tig" -o "${OPTYPE}" = "all" ]; then
-    cd ${CUR_DIR}/tools
+    cd ${ROOT_DIR}/tools
     tar -xzf libiconv-1.15.tar.gz
     cd libiconv-1.15
+
     ./configure --prefix=/usr
-    make && make install
     if [ $? -ne 0 ]; then
         exit -1
     fi
 
-    cd ${CUR_DIR}/tools
+    make -j 2
+    if [ $? -ne 0 ]; then
+        exit -1
+    fi
+
+    make install
+    if [ $? -ne 0 ]; then
+        exit -1
+    fi
+
+    cd ${ROOT_DIR}/tools
     rm -fr libiconv-1.15
 
     IS_INSTALL=`rpm -qa | grep ncurses-devel`
     if [ -z "${IS_INSTALL}" ]; then
         rpm -ivh ncurses-devel-5.9-13.20130511.el7.x86_64.rpm --nodeps --force
+        if [ $? -ne 0 ]; then
+            exit -1
+        fi
     fi
 
     IS_INSTALL=`rpm -qa | grep ncurses-libs`
     if [ -z "${IS_INSTALL}" ]; then
         rpm -ivh ncurses-libs-5.9-13.20130511.el7.x86_64.rpm --nodeps --force
+        if [ $? -ne 0 ]; then
+            exit -1
+        fi
     fi
 
     tar -xzf tig-2.3.3.tar.gz
     cd tig-2.3.3
+
     make configure
     ./configure --prefix=/usr
-    make && make install
     if [ $? -ne 0 ]; then
         exit -1
     fi
 
-    cd ${CUR_DIR}/tools
+    make -j 2
+    if [ $? -ne 0 ]; then
+        exit -1
+    fi
+
+    make install
+    if [ $? -ne 0 ]; then
+        exit -1
+    fi
+
+    cd ${ROOT_DIR}/tools
     rm -fr tig-2.3.3
 fi
 
 # prepare environment
 if [ "${OPTYPE}" = "env" -o "${OPTYPE}" = "all" ]; then
     if [ ! -f ~/.vimrc ]; then
-        ln -s ${CAMP_DIR}/vimrc ~/.vimrc
+        ln -s ${ROOT_DIR}/vimrc ~/.vimrc
     fi
 
     if [ ! -f ~/.bashrc ]; then
-        ln -s ${CAMP_DIR}/bashrc ~/.bashrc
+        ln -s ${ROOT_DIR}/bashrc ~/.bashrc
     fi
 
     if [ ! -f ~/.bash_profile ]; then
-        ln -s ${CAMP_DIR}/bash_profile ~/.bash_profile
+        ln -s ${ROOT_DIR}/bash_profile ~/.bash_profile
     fi
 
     if [ ! -f ~/.minttyrc ]; then
-        ln -s ${CAMP_DIR}/minttyrc ~/.minttyrc
+        ln -s ${ROOT_DIR}/minttyrc ~/.minttyrc
     fi
 
     if [ ! -f ~/.inputrc ]; then
-        ln -s ${CAMP_DIR}/inputrc ~/.inputrc
+        ln -s ${ROOT_DIR}/inputrc ~/.inputrc
     fi
 
     if [ ! -f ~/.astylerc ]; then
-        ln -s ${CAMP_DIR}/astylerc ~/.astylerc
+        ln -s ${ROOT_DIR}/astylerc ~/.astylerc
     fi
 
     # source environment config
@@ -133,8 +174,8 @@ if [ "${OPTYPE}" = "env" -o "${OPTYPE}" = "all" ]; then
 
     # build vim-work environment
     mkdir -p ~/.vim
-    cp -fr ${CAMP_DIR}/colors ~/.vim
-    cp -fr ${CAMP_DIR}/syntax ~/.vim
+    cp -fr ${ROOT_DIR}/colors ~/.vim
+    cp -fr ${ROOT_DIR}/syntax ~/.vim
 
     # install bundle plugin
     if [ ! -d ~/.vim/bundle/vundle ]; then
@@ -144,11 +185,11 @@ fi
 
 # install astyle
 if [ "${OPTYPE}" = "astyle" -o "${OPTYPE}" = "all" ]; then
-    cd ${CUR_DIR}/tools
+    cd ${ROOT_DIR}/tools
     tar -xzf astyle_*.tar.gz
-
     cd astyle/build/gcc
-    make
+
+    make -j 2
     if [ $? -ne 0 ]; then
         exit -1
     fi
@@ -156,13 +197,13 @@ if [ "${OPTYPE}" = "astyle" -o "${OPTYPE}" = "all" ]; then
     cp -f bin/astyle* /usr/bin/
     chmod 777 /usr/bin/astyle*
 
-    cd ${CUR_DIR}/tools
+    cd ${ROOT_DIR}/tools
     rm -fr astyle/
 fi
 
 # install ack-grep
 if [ "${OPTYPE}" = "astyle" -o "${OPTYPE}" = "all" ]; then
-    cd ${CUR_DIR}/tools
+    cd ${ROOT_DIR}/tools
     cp -f ack-* /usr/bin/ack-grep
     chmod 777 /usr/bin/ack-grep
 fi
