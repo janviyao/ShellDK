346161d517bd5719e713c1dea6886cf64bfc59c9
Janvi Yao <9971289@qq.com>
2022-03-01 11:20:37 +0800

    fix log.sh

--- tools/log.sh
+++ tools/log.sh
@@ -4,15 +4,14 @@ IFS_BACKUP=$IFS
 IFS=$(echo -en "\n\r")
 
 if bool_v "${DEBUG_ON}";then
+    echo $*
+    $*
+else
     if var_exist "LOG_FILE";then
         $* >> ${LOG_FILE} 2>&1
     else
         $* >> log 2>&1
     fi
-else
-    echo $*
-    $*
-    echo ""
 fi
 errcode=$?
 
