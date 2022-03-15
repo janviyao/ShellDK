9c0c6574e204a37f94343d1b92bca7dbb5f79fca
Janvi Yao <9971289@qq.com>
2022-03-15 10:21:00 +0800

    add test_app_log env
    Signed-off-by: Janvi Yao <9971289@qq.com>

--- tools/iscsi/istgt/setup.sh
+++ tools/iscsi/istgt/setup.sh
@@ -4,9 +4,10 @@ ISTGT_ROOT_DIR=$(current_filedir)
 
 ISTGT_APP_NAME=istgt
 ISTGT_SRC_ROOT=/root/istgt
-ISTGT_LOG_DIR=/apsarapangu/fastdisk/${ISTGT_APP_NAME}
 ISTGT_APP_DIR=${ISTGT_SRC_ROOT}/src
-ISTGT_ISCSI_APP="${ISTGT_APP_DIR}/${ISTGT_APP_NAME}"
+ISTGT_LOG_DIR=/apsarapangu/fastdisk/${ISTGT_APP_NAME}
+ISTGT_APP_LOG=${ISTGT_LOG_DIR}/${ISTGT_APP_NAME}.log
+ISTGT_ISCSI_APP="${ISTGT_APP_DIR}/${ISTGT_APP_NAME} &> ${ISTGT_APP_LOG}"
 
 #ISCSI_NODE_BASE=iqn.2007-09.jp.ne.peach.istgt
 ISCSI_NODE_BASE=$(cat ${ISTGT_ROOT_DIR}/conf/istgt.conf | grep -P "^\s*NodeBase\s+" | awk '{ print $2 }' | grep -P "[0-9a-zA-Z\-\.]+" -o)
@@ -15,8 +16,9 @@ ISCSI_TARGET_NAME=($(cat ${ISTGT_ROOT_DIR}/conf/istgt.conf | grep -P "^\s*Target
 config_add "${TEST_SUIT_ENV}" "ISCSI_NODE_BASE" "${ISCSI_NODE_BASE}"
 config_add "${TEST_SUIT_ENV}" "declare -a ISCSI_TARGET_NAME" "(${ISCSI_TARGET_NAME[*]})"
 
+config_add "${TEST_SUIT_ENV}" "TEST_APP_NAME"    "${ISTGT_APP_NAME}"
 config_add "${TEST_SUIT_ENV}" "TEST_APP_SRC"     "${ISTGT_SRC_ROOT}"
 config_add "${TEST_SUIT_ENV}" "TEST_APP_DIR"     "${ISTGT_APP_DIR}"
-config_add "${TEST_SUIT_ENV}" "TEST_APP_NAME"    "${ISTGT_APP_NAME}"
 config_add "${TEST_SUIT_ENV}" "TEST_LOG_DIR"     "${ISTGT_LOG_DIR}"
+config_add "${TEST_SUIT_ENV}" "TEST_APP_LOG"     "${ISTGT_APP_LOG}"
 config_add "${TEST_SUIT_ENV}" "TEST_APP_RUNTIME" "\"${ISTGT_ISCSI_APP}\""
--- tools/iscsi/spdk/setup.sh
+++ tools/iscsi/spdk/setup.sh
@@ -4,8 +4,9 @@ SPDK_ROOT_DIR=$(current_filedir)
 SPDK_APP_NAME=iscsi_tgt
 SPDK_SRC_ROOT=/apsarapangu/develop/FusionTarget
 SPDK_LOG_DIR=/apsarapangu/fastdisk/${SPDK_APP_NAME}
+SPDK_APP_LOG=${SPDK_LOG_DIR}/${SPDK_APP_NAME}.log
 SPDK_APP_DIR=${SPDK_SRC_ROOT}/app/iscsi_tgt
-SPDK_ISCSI_APP="${SPDK_APP_DIR}/${SPDK_APP_NAME} -c ${SPDK_ROOT_DIR}/conf/iscsi.conf.in -m 0XFF --shm-id=1 --logflag iscsi --logflag scsi --logflag bdev --logflag bdev_malloc &> ${SPDK_LOG_DIR}/${SPDK_APP_NAME}.log"
+SPDK_ISCSI_APP="${SPDK_APP_DIR}/${SPDK_APP_NAME} -c ${SPDK_ROOT_DIR}/conf/iscsi.conf.in -m 0XFF --shm-id=1 --logflag iscsi --logflag scsi --logflag bdev --logflag bdev_malloc &> ${SPDK_APP_LOG}"
 
 #ISCSI_NODE_BASE=iqn.2016-06.io.spdk
 ISCSI_NODE_BASE=$(cat ${SPDK_ROOT_DIR}/conf/iscsi.conf.in | grep -P "^\s*NodeBase\s+" | awk '{ print $2 }' | grep -P "[0-9a-zA-Z\-\.]+" -o)
@@ -14,8 +15,9 @@ ISCSI_TARGET_NAME=($(cat ${SPDK_ROOT_DIR}/conf/iscsi.conf.in | grep -P "^\s*Targ
 config_add "${TEST_SUIT_ENV}" "ISCSI_NODE_BASE" "${ISCSI_NODE_BASE}"
 config_add "${TEST_SUIT_ENV}" "declare -a ISCSI_TARGET_NAME" "(${ISCSI_TARGET_NAME[*]})"
 
+config_add "${TEST_SUIT_ENV}" "TEST_APP_NAME"    "${SPDK_APP_NAME}"
 config_add "${TEST_SUIT_ENV}" "TEST_APP_SRC"     "${SPDK_SRC_ROOT}"
 config_add "${TEST_SUIT_ENV}" "TEST_APP_DIR"     "${SPDK_APP_DIR}"
 config_add "${TEST_SUIT_ENV}" "TEST_LOG_DIR"     "${SPDK_LOG_DIR}"
-config_add "${TEST_SUIT_ENV}" "TEST_APP_NAME"    "${SPDK_APP_NAME}"
+config_add "${TEST_SUIT_ENV}" "TEST_APP_LOG"     "${SPDK_APP_LOG}"
 config_add "${TEST_SUIT_ENV}" "TEST_APP_RUNTIME" "\"${SPDK_ISCSI_APP}\""
