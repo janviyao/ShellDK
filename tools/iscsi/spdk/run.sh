6061a3cca53c9641ee336a6f082d12e57399a102
Janvi Yao <9971289@qq.com>
2022-03-16 13:54:38 +0800

    optimize for iscsi debug save
    Signed-off-by: Janvi Yao <9971289@qq.com>

--- tools/iscsi/spdk/run.sh
+++ tools/iscsi/spdk/run.sh
@@ -24,7 +24,7 @@ fi
 mkdir -p ${TEST_LOG_DIR}
 
 DATE_TIME=$(date '+%Y%m%d-%H%M%S')
-${ISCSI_ROOT_DIR}/${TEST_TARGET}/save_debug.sh "${TEST_LOG_DIR}/coredump/${DATE_TIME}"
+${ISCSI_ROOT_DIR}/${TEST_TARGET}/save_debug.sh "${TEST_LOG_DIR}/debug/${DATE_TIME}"
 
 if process_exist "${TEST_APP_NAME}";then
     ${TOOL_ROOT_DIR}/stop_p.sh KILL "${TEST_APP_NAME}"
