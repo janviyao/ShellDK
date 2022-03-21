9660ba140373ceba813a3f5458e0e8821851ffcb
Janvi Yao <9971289@qq.com>
2022-03-21 20:39:50 +0800

    fix configure
    Signed-off-by: Janvi Yao <9971289@qq.com>

--- tools/iscsi/custom/configure.sh
+++ tools/iscsi/custom/configure.sh
@@ -19,21 +19,23 @@ fi
 LOCAL_CONF="${ISCSI_ROOT_DIR}/${TEST_TARGET}/conf/iscsi.conf.in"
 can_access "${APP_CONF_DIR}" || ${SUDO} "mkdir -p ${APP_CONF_DIR}"
 can_access "${LOCAL_CONF}" && ${SUDO} cp -f ${LOCAL_CONF} ${APP_CONF_DIR}
-${SUDO} chmod -R 755 ${APP_CONF_DIR}
+${SUDO} chmod -R 777 ${APP_CONF_DIR}
 
 sed -i "s/${keystr}=.\+/${keystr}=${valstr}/g" ${APP_CONF_DIR}/iscsi.conf.in
 
 for ipaddr in ${ISCSI_TARGET_IP_ARRAY[*]}
 do
     sed -i "s#Portal\s\+\(.*\)\s\+[0-9]\+\.[0-9]\+\.[0-9]\+\.[0-9]\+:3260\+#Portal \1 ${ipaddr}:3260#g" ${APP_CONF_DIR}/iscsi.conf.in
+    echo_info "update: Portal ${ipaddr}:3260"
 done
 
 for ipaddr in ${ISCSI_INITIATOR_IP_ARRAY[*]}
 do
     match_ln=$(sed -n "\#Netmask\s\+[0-9]\+\.[0-9]\+\.[0-9]\+\.0/[0-9]\+#=" ${APP_CONF_DIR}/iscsi.conf.in | tail -n 1)
-    if [ -z "${match_ln}" ];then
+    if [ -n "${match_ln}" ];then
         netmask=$(string_regex "${ipaddr}" "^\d+\.\d+\.\d+")
         sed -i "${match_ln}a\  Netmask ${netmask}.0/24" ${APP_CONF_DIR}/iscsi.conf.in
+        echo_info "add: Netmask ${netmask}.0/24 @${match_ln}"
     fi
 done
 
