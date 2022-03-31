5eb109f166bfdafaa0938ef1a0b3e59864c584fd
Janvi Yao <9971289@qq.com>
2022-03-31 21:17:57 +0800

    optimize for test
    Signed-off-by: Janvi Yao <9971289@qq.com>

--- tools/iscsi/save_initiator_debug.sh
+++ tools/iscsi/save_initiator_debug.sh
@@ -2,19 +2,21 @@
 source ${TEST_SUIT_ENV} 
 echo_info "@@@@@@: $(path2fname $0) @${LOCAL_IP}"
 
+${SUDO} "mkdir -p ${INITIATOR_LOG_DIR}; chmod -R 777 ${INITIATOR_LOG_DIR}"
+
 if bool_v "${KERNEL_DEBUG_ON}";then
     echo_info "Save: dmesg"
-    dmesg &> ${TEST_LOG_DIR}/initiator.dmesg.log 
+    dmesg &> ${INITIATOR_LOG_DIR}/initiator.dmesg.log 
 fi
 
 if can_access "${ISCSI_INITIATOR_LOG}";then
     echo_info "Save: ${ISCSI_INITIATOR_LOG}"
-    if ! match_str_start "${ISCSI_INITIATOR_LOG}" "${TEST_LOG_DIR}";then
-        ${SUDO} mv -f ${ISCSI_INITIATOR_LOG}* ${TEST_LOG_DIR}
+    if ! match_str_start "${ISCSI_INITIATOR_LOG}" "${INITIATOR_LOG_DIR}";then
+        ${SUDO} mv -f ${ISCSI_INITIATOR_LOG}* ${INITIATOR_LOG_DIR}
     fi
 fi
 
 if [[ ${LOCAL_IP} != ${CONTROL_IP} ]];then
-    echo_info "Push { ${TEST_LOG_DIR} } to { ${CONTROL_IP} }"
-    rsync_to ${TEST_LOG_DIR} ${CONTROL_IP}
+    echo_info "Push { ${INITIATOR_LOG_DIR} } to { ${CONTROL_IP} }"
+    rsync_to ${INITIATOR_LOG_DIR} ${CONTROL_IP}
 fi
