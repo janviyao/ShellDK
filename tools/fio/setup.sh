678781827769c4e7d3c5a0177ed4cc3265b7722f
Janvi Yao <9971289@qq.com>
2022-03-31 10:14:08 +0800

    fix fio setup
    Signed-off-by: Janvi Yao <9971289@qq.com>

--- tools/fio/setup.sh
+++ tools/fio/setup.sh
@@ -10,7 +10,7 @@ FIO_RESULT_FILE=${FIO_OUTPUT_DIR}/result.csv
 
 kvconf_add "${TEST_SUIT_ENV}" "FIO_ROOT_DIR"    "${FIO_ROOT_DIR}"
 kvconf_add "${TEST_SUIT_ENV}" "FIO_CONF_DIR"    "${FIO_CONF_DIR}"
-kvconf_add "${TEST_SUIT_ENV}" "FIO_APP_RUNTIME" "${FIO_APP_RUNTIME}"
+kvconf_add "${TEST_SUIT_ENV}" "FIO_APP_RUNTIME" "\"${FIO_APP_RUNTIME}\""
 kvconf_add "${TEST_SUIT_ENV}" "FIO_OUTPUT_DIR"  "${FIO_OUTPUT_DIR}"
 kvconf_add "${TEST_SUIT_ENV}" "FIO_RESULT_FILE" "${FIO_RESULT_FILE}"
 
