91e0479950473fb31fed4c2d942a139d350be26b
Janvi Yao <9971289@qq.com>
2022-06-22 17:44:14 +0800

    fix xfer
    Signed-off-by: Janvi Yao <9971289@qq.com>

--- tools/task/xfer_task.sh
+++ tools/task/xfer_task.sh
@@ -53,7 +53,7 @@ function do_rsync
                     sync_cmd="\
                     export USR_PASSWORD='${USR_PASSWORD}';\
                     if ls '${MY_VIM_DIR}' &> /dev/null;then\
-                        source $MY_VIM_DIR/tools/include/system.api.sh;\
+                        source $MY_VIM_DIR/tools/include/common.api.sh;\
                         if ! test -d '${xfer_dir}';then\
                             sudo_it mkdir -p '${xfer_dir}';\
                             sudo_it chmod +w '${xfer_dir}';\
