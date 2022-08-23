fbdd19ab5fba275253143d0b5dba0ce8a316a8f3
Janvi Yao <9971289@qq.com>
2022-08-23 15:25:46 +0800

    opf kvconf
    Signed-off-by: Janvi Yao <9971289@qq.com>

--- tools/include/kvconf.api.sh
+++ tools/include/kvconf.api.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+KV_FS="="
 function kvconf_has_key
 {
     local kv_file="$1"
@@ -13,7 +14,7 @@ function kvconf_has_key
         return 1
     fi 
     
-    if grep -P "^\s*${key_str}\s*=" ${kv_file} &>/dev/null;then
+    if grep -P "^\s*${key_str}\s*${KV_FS}" ${kv_file} &>/dev/null;then
         return 0
     else
         return 1
@@ -55,15 +56,15 @@ function kvconf_set
     fi
 
     if ! can_access "${kv_file}";then
-        return 1
+        echo > ${kv_file}
     fi 
     
     if kvconf_has_key "${kv_file}" "${key_str}";then
         key_str=$(replace_regex "${key_str}" "/" "\/")
         val_str=$(replace_regex "${val_str}" "/" "\/")
-        sed -i "s/${key_str}=.\+/${key_str}=${val_str}/g" ${kv_file}
+        sed -i "s/${key_str}${KV_FS}.\+/${key_str}${KV_FS}${val_str}/g" ${kv_file}
     else
-        sed -i "\$a\\${key_str}=${val_str}" ${kv_file}
+        sed -i "\$a\\${key_str}${KV_FS}${val_str}" ${kv_file}
     fi
 
     local retcode=$?
@@ -84,9 +85,9 @@ function kvconf_get
         return 1
     fi 
     
-    local val_str=$(grep -P "^\s*${key_str}\s*=" ${kv_file})
+    local val_str=$(grep -P "^\s*${key_str}\s*${KV_FS}" ${kv_file})
     if [ -n "${val_str}" ];then
-        val_str=$(replace_regex "${val_str}" "^\s*${key_str}\s*=" "")
+        val_str=$(replace_regex "${val_str}" "^\s*${key_str}\s*${KV_FS}" "")
         local val_array=($(echo "${val_str}" | tr ',' ' '))
         echo "${val_array[*]}"
     else
@@ -106,7 +107,7 @@ function kvconf_append
     fi
 
     if ! can_access "${kv_file}";then
-        return 1
+        echo > ${kv_file}
     fi 
    
     local old_val=$(kvconf_get "${kv_file}" "${key_str}")
@@ -114,9 +115,9 @@ function kvconf_append
         key_str=$(replace_regex "${key_str}" "/" "\/")
         val_str=$(replace_regex "${val_str}" "/" "\/")
         val_str="${old_val},${val_str}"
-        sed -i "s/${key_str}=.\+/${key_str}=${val_str}/g" ${kv_file}
+        sed -i "s/${key_str}${KV_FS}.\+/${key_str}${KV_FS}${val_str}/g" ${kv_file}
     else
-        sed -i "\$a\\${key_str}=${val_str}" ${kv_file}
+        sed -i "\$a\\${key_str}${KV_FS}${val_str}" ${kv_file}
     fi
 
     local retcode=$?
@@ -136,10 +137,10 @@ function kvconf_insert
     fi
 
     if ! can_access "${kv_file}";then
-        return 1
+        echo > ${kv_file}
     fi 
     
-    sed -i "${line_nr}i\\${key_str}=${val_str}" ${kv_file}
+    sed -i "${line_nr}i\\${key_str}${KV_FS}${val_str}" ${kv_file}
     
     local retcode=$?
     return ${retcode}
@@ -161,7 +162,7 @@ function kvconf_del
     
     if kvconf_has_key "${kv_file}" "${key_str}";then
         key_str=$(replace_regex "${key_str}" "/" "\/")
-        sed -i "/${key_str}.*=.\+$/d" ${kv_file}
+        sed -i "/${key_str}.*${KV_FS}.\+$/d" ${kv_file}
     fi
 
     local retcode=$?
@@ -199,7 +200,6 @@ function kvconf_line_nr
     fi
 
     if ! can_access "${kv_file}";then
-        echo ""
         return 1
     fi 
     
@@ -209,7 +209,7 @@ function kvconf_line_nr
     fi
     
     key_str=$(replace_regex "${key_str}" "/" "\/")
-    local line_nr=$(sed -n "/^\s*${key_str}\s*=/{=;q;}" ${kv_file})
+    local line_nr=$(sed -n "/^\s*${key_str}\s*${KV_FS}/{=;q;}" ${kv_file})
     echo "${line_nr}"
     return 0
 }
