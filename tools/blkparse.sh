b4fd4a561a6e59f426b628fdc2d8dc09e787cde9
Janvi Yao <janviyao@qq.com>
2022-09-14 14:53:12 +0800

    opf blkparse.sh
    Signed-off-by: Janvi Yao <janviyao@qq.com>

--- tools/blkparse.sh
+++ tools/blkparse.sh
@@ -1,108 +1,108 @@
 #!/bin/bash
-CUR_DIR=$(pwd)
+function how_use
+{
+    local script_name=$(path2fname $0)
+    echo "=================== Usage ==================="
+    printf "%-15s <device-name> <trace-time> <output-dir>\n" "${script_name}"
+    printf "%-15s @%s\n" "<device-name>" "name of device traced, such as sdb,sdc..."
+    printf "%-15s @%s\n" "<trace-time>"  "time traced"
+    printf "%-15s @%s\n" "<output-dir>"  "directory where result output"
+    echo "============================================="
+}
 
-DEVICE=$1
-TIME_S=$2
-OUTPUT=$3
-
-FNAME=${DEVICE}.blktrace
-PREFIX="******"
-
-if [ -z "${TIME_S}" ];then
-    TIME_S=60
+if [ $# -lt 1 ];then
+    how_use
+    exit 1
 fi
 
-LBA_S=1
-LBA_E=3
+device_name=$1
+traced_time=$2
+output_dir=$3
 
-if [ -z "${OUTPUT}" ];then
-    OUTPUT=${CUR_DIR}/result
+if [ -z "${traced_time}" ];then
+    traced_time=60
+fi
+
+CUR_DIR=$(pwd)
+if [ -z "${output_dir}" ];then
+    output_dir=${CUR_DIR}/blktrace
     try_cnt=0
-    tmp_dir=${OUTPUT}
+    tmp_dir=${output_dir}
     while can_access "${tmp_dir}"
     do
         let try_cnt++
-        tmp_dir=${OUTPUT}${try_cnt}
+        tmp_dir=${output_dir}${try_cnt}
     done
-    OUTPUT=${tmp_dir}
+    output_dir=${tmp_dir}
 else
-    $SUDO rm -fr ${OUTPUT}
+    sudo_it rm -fr ${output_dir}
 fi
+mkdir -p ${output_dir}/${device_name}
 
-mkdir -p ${OUTPUT}/${DEVICE}
-cd ${OUTPUT}
-
-$SUDO blktrace -w ${TIME_S} -d /dev/${DEVICE} -o ${DEVICE}/${DEVICE} 
-if [ $? -eq 0 ];then
-    echo "${PREFIX} blktrace into ${DEVICE}/${DEVICE}"
-else
-    echo "${PREFIX} blktrace fail"
+cd ${output_dir}
+sudo_it blktrace -w ${traced_time} -d /dev/${device_name} -o ${device_name}/${device_name} &> /dev/null
+if [ $? -ne 0 ];then
+    echo_erro "blktrace fail"
+    cd ${CUR_DIR}
     exit 1
 fi
+echo_info "blktrace into { ${output_dir}/${device_name}/${device_name} }"
 
-blkparse -i ${DEVICE}/${DEVICE} -d ${FNAME}.bin > ${FNAME}
-if [ $? -eq 0 ];then
-    echo "${PREFIX} blkparse into ${FNAME}"
-else
-    echo "${PREFIX} blkparse fail"
+blkparse -i ${device_name}/${device_name} -d blktrace.bin > blktrace
+if [ $? -ne 0 ];then
+    echo_erro "blkparse fail"
+    cd ${CUR_DIR}
     exit 1
 fi
+echo_info "blkparse into { ${output_dir}/blktrace }"
 
-btt -A -I ${FNAME}.iostat -Q ${FNAME}.aqd -m ${FNAME}.seek -s ${FNAME}.seek -B ${FNAME}.bno -i ${FNAME}.bin -o ${FNAME}.btt
-if [ $? -eq 0 ];then
-    echo "${PREFIX} btt into ${FNAME}.btt"
-else
-    echo "${PREFIX} btt fail"
+btt -A -I blktrace.iostat -Q blktrace.aqd -m blktrace.seek -s blktrace.seek -B blktrace.bno -i blktrace.bin -o blktrace.btt
+if [ $? -ne 0 ];then
+    echo_erro "btt fail"
+    cd ${CUR_DIR}
     exit 1
 fi
+echo_info "btt into { ${output_dir}/blktrace.btt }"
 
-cat ${FNAME} | grep -E " D " | awk '{ if( $9=="+" ) { print $4":"$8" + "$10 } }' | sort -k 1n -u > ${FNAME}.lba
-echo "${PREFIX} generate ${FNAME}.lba"
+cat blktrace | grep -E " D " | awk '{ if( $9=="+" ) { print $4":"$8" + "$10 } }' | sort -k 1n -u > blktrace.lba
+echo_info "generate { ${output_dir}/blktrace.lba }"
 
-cat /dev/null > ${FNAME}.sort
-cat /dev/null > ${FNAME}.dc
+cat /dev/null > blktrace.sort
+cat /dev/null > blktrace.dc
 
-echo "${PREFIX} collect D|C from ${FNAME} to ${FNAME}.dc"
+echo_info "collect [D|C] from { ${output_dir}/blktrace } to { ${output_dir}/blktrace.dc }"
 while read lba_line
 do
-    lba_tim=$(echo "${lba_line}" | awk -F: '{ print $1 }')
     lba_exp=$(echo "${lba_line}" | awk -F: '{ print $2 }')
-    lba_val=$(FLOAT "${lba_exp}" 0)
-    
-    #echo "${lba_tim} >= ${LBA_S} && ${lba_tim} <= ${LBA_E}"
-    if EXPR_IF "${lba_tim} >= ${LBA_S}" && EXPR_IF "${lba_tim} <= ${LBA_E}";then
-        related_line=$(cat ${FNAME} | grep " ${lba_exp} " | sed "s/[ ]\+/ /g" | sort -n -k 4)
-        echo "${related_line}" >> ${FNAME}.sort 
 
-        dc_content=$(echo "${related_line}" | grep -E " D | C ")
-        #echo "${dc_content}"
-        echo "${dc_content}" >> ${FNAME}.dc
-    fi
+    related_line=$(cat blktrace | grep " ${lba_exp} " | sed "s/[ ]\+/ /g" | sort -n -k 4)
+    echo "${related_line}" >> blktrace.sort 
 
-    if EXPR_IF "${lba_tim} > ${LBA_E}";then
-        break
-    fi
-done < ${FNAME}.lba
-echo "${PREFIX} generate ${FNAME}.dc"
+    dc_content=$(echo "${related_line}" | grep -E " D | C ")
+    #echo "${dc_content}"
+    echo "${dc_content}" >> blktrace.dc
+done < blktrace.lba
+echo_info "generate { ${output_dir}/blktrace.dc }"
 
-cat /dev/null > ${FNAME}.dc.diff
+cat /dev/null > blktrace.dc.diff
+echo_info "collect dc.diff from { ${output_dir}/blktrace.dc } to { ${output_dir}/blktrace.dc.diff }"
 
-echo "${PREFIX} collect dc.diff from ${FNAME}.dc to ${FNAME}.dc.diff"
-line_nr=$(cat ${FNAME}.dc | wc -l)
+line_nr=$(cat blktrace.dc | wc -l)
 for ((index=1; index<=${line_nr};))
 do
-    frt_idx=$index
+    frt_idx=${index}
     let snd_idx=index+1
 
-    frt_line=$(sed -n "${frt_idx}p" ${FNAME}.dc)
-    snd_line=$(sed -n "${snd_idx}p" ${FNAME}.dc)
+    frt_line=$(sed -n "${frt_idx}p" blktrace.dc)
+    snd_line=$(sed -n "${snd_idx}p" blktrace.dc)
     
     frt_flag=$(echo "${frt_line}" | awk '{ print $6 }')
     snd_flag=$(echo "${snd_line}" | awk '{ print $6 }')
+
     if [ "${frt_flag}" != "D" -o "${snd_flag}" != "C" ];then
-        echo "exception:"
-        echo "Line: ${frt_idx} Content: ${frt_line}"
-        echo "Line: ${snd_idx} Content: ${snd_line}"
+        echo_erro "exception:"
+        echo_erro "Line: ${frt_idx} Content: ${frt_line}"
+        echo_erro "Line: ${snd_idx} Content: ${snd_line}"
         ((index+=1))
         continue
     fi
@@ -111,16 +111,16 @@ do
     #echo "${snd_line}"
     frt_val=$(echo "${frt_line}" | awk '{ print $4 }')
     snd_val=$(echo "${snd_line}" | awk '{ print $4 }')
+    dif_val=$(FLOAT "(${snd_val}-${frt_val})*1000" 9)
 
-    dif_val=$(printf "%.9f" $(echo "scale=9;(${snd_val}-${frt_val})*1000"|bc))
-
-    echo "${snd_val} - ${frt_val} = ${dif_val}"
-    echo "${snd_val} - ${frt_val} = ${dif_val}" >> ${FNAME}.dc.diff
+    #echo "${snd_val} - ${frt_val} = ${dif_val}"
+    echo "${snd_val} - ${frt_val} = ${dif_val}" >> blktrace.dc.diff
 
     ((index+=2))
 done
-echo "${PREFIX} generate ${FNAME}.dc.diff"
+echo_info "generate { ${output_dir}/blktrace.dc.diff }"
 
-cat ${FNAME}.dc.diff | sort -k 5nr | head -n 30 > ${FNAME}.dc.max
-echo "${PREFIX} generate ${FNAME}.dc.max"
-cd $CUR_DIR
+cat blktrace.dc.diff | sort -k 5nr | head -n 30 > blktrace.dc.max
+echo_info "generate { ${output_dir}/blktrace.dc.max }"
+
+cd ${CUR_DIR}
