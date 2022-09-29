c600fb8b52ad58efba0c2ff01f5c15d6d569119e
Janvi Yao <janviyao@qq.com>
2022-09-29 16:08:28 +0800

    opf netperf
    Signed-off-by: Janvi Yao <janviyao@qq.com>

--- tools/netperf.sh
+++ tools/netperf.sh
@@ -14,8 +14,10 @@ if [ $# -lt 1 ];then
     exit 1
 fi
 
+test_time=60
 client_ip="$1"
 server_ip="$2"
+
 if [ $# -eq 1 ];then
     client_ip="${LOCAL_IP}"
     server_ip="$1"
@@ -32,16 +34,15 @@ if ! match_regex "${server_ip}" "\d+\.\d+\.\d+\.\d+";then
     exit 1
 fi
 
-test_time=60
+${MY_VIM_DIR}/tools/sshlogin.sh "${server_ip}" "process_kill netserver" 
+${MY_VIM_DIR}/tools/sshlogin.sh "${server_ip}" "nohup netserver &" 
 
-${MY_VIM_DIR}/tools/sshlogin.sh "${server_ip}" "process_kill netserver && nohup netserver &" 
-
-# 带宽测试：client向server发送 32KB 大包：
+# 带宽测试：client向server发送 1024KB 大包：
 ${MY_VIM_DIR}/tools/sshlogin.sh "${client_ip}" "netperf -t TCP_STREAM -H ${server_ip} -l ${test_time} -- -m 1024k 'MAX_LATENCY,MEAN_LATENCY,P90_LATENCY,P99_LATENCY,P999_LATENCY,P9999_LATENCY,STDDEV_LATENCY,THROUGHPUT,THROUGHPUT_UNITS'"
-
 # 延迟测试
 # 长连接：
 ${MY_VIM_DIR}/tools/sshlogin.sh "${client_ip}" "netperf -t TCP_RR -H ${server_ip} -l ${test_time} -- -r 4k -O 'MIN_LAETENCY,MAX_LATENCY,MEAN_LATENCY,P90_LATENCY,P99_LATENCY,P999_LATENCY,P9999_LATENCY,STDDEV_LATENCY,THROUGHPUT,THROUGHPUT_UNITS'"
-
 # 短连接：
 ${MY_VIM_DIR}/tools/sshlogin.sh "${client_ip}" "netperf -t TCP_CRR -H ${server_ip} -l ${test_time} -- -r 4k -O 'MIN_LAETENCY,MAX_LATENCY,MEAN_LATENCY,P90_LATENCY,P99_LATENCY,P999_LATENCY,P9999_LATENCY,STDDEV_LATENCY,THROUGHPUT,THROUGHPUT_UNITS'"
+
+${MY_VIM_DIR}/tools/sshlogin.sh "${server_ip}" "process_kill netserver" 
