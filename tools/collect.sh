f409f4a7475a922484a5453bf876d16c08a7df30
Janvi Yao <janviyao@qq.com>
2025-06-17 20:46:25 +0800

    opf collect.sh
    Signed-off-by: Janvi Yao <janviyao@qq.com>

--- tools/collect.sh
+++ tools/collect.sh
@@ -1,36 +1,22 @@
 #!/bin/bash
-#set -x
 echo_info "@@@@@@: $(file_fname_get $0) @${LOCAL_IP}"
-
-EXPORT_FILE="$1"
-if [ -z "${EXPORT_FILE}" ];then
-    EXPORT_FILE="${MY_HOME}/vim.tar"
-fi
-
-EXPORT_DIR=$(file_path_get "${EXPORT_FILE}")
-if ! file_exist "${EXPORT_DIR}";then
-    ${SUDO} "mkdir -p ${EXPORT_DIR}; chmod -R 777 ${EXPORT_DIR}" 
-fi
+EXPORT_FILE="$(pwd)/vim.tar"
 
 # Collect dirs and files 
-TAR_WHAT="${MY_VIM_DIR}"
-TAR_WHAT="${TAR_WHAT} ${MY_HOME}/.ssh"
-TAR_WHAT="${TAR_WHAT} ${MY_HOME}/.vim"
-#TAR_WHAT="${TAR_WHAT} ${MY_HOME}/.vimrc"
-#TAR_WHAT="${TAR_WHAT} ${MY_HOME}/.bashrc"
-#TAR_WHAT="${TAR_WHAT} ${MY_HOME}/.bash_profile"
-#TAR_WHAT="${TAR_WHAT} ${MY_HOME}/.minttyrc"
-#TAR_WHAT="${TAR_WHAT} ${MY_HOME}/.inputrc"
-#TAR_WHAT="${TAR_WHAT} ${MY_HOME}/.astylerc"
+declare -a TAR_WHAT
+TAR_WHAT+=("${MY_VIM_DIR}")
+TAR_WHAT+=("${MY_HOME}/.ssh")
+TAR_WHAT+=("${MY_HOME}/.vim")
+TAR_WHAT+=("$@")
 
 # Start to tar 
-file_exist "${EXPORT_FILE}" && ${SUDO} rm -f ${EXPORT_FILE}
-for item in ${TAR_WHAT}
+file_exist "${EXPORT_FILE}" && sudo_it rm -f ${EXPORT_FILE}
+for item in "${TAR_WHAT[@]}"
 do
     TAR_DIR=$(file_path_get "${item}")
     TAR_FILE=$(file_fname_get "${item}")
 
-    echo_info "Collect { $(printf -- '%-20s' "${item}") } into { ${EXPORT_FILE} }"
+    echo_info "Collect { $(printf -- '%-30s' "${item}") } into { ${EXPORT_FILE} }"
 
     cd ${TAR_DIR}
     if file_exist "${EXPORT_FILE}";then
